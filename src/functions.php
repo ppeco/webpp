f1213b037ac1b044e41cb6e9d02098d14965d5eb
Лев Копорушкин <levkopoag@gmail.com>
2021-08-25 13:18:16 +0500

    Update

--- src/functions.php
+++ src/functions.php
@@ -2,6 +2,7 @@
 
 use ppeco\tplpp\Template;
 
-function view(string $name): Template {
-    return Template::fromFile(__DIR__.'/../resources/templates/'.$name.'.tpl');
+function view(string $name, array $parameters = []): Template {
+    return Template::fromFile(__DIR__.'/../resources/templates/'.$name.'.tpl')
+        ->addValues($parameters);
 }
